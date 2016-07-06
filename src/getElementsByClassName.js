5aa01e6c31c3121d5123934e6e6d8d545c24d9f7
Daniel Metzger <kinaharo@gmail.com>
2016-07-05 19:13:44 -0700

    Return the same results as getElementsByClassName

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,19 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
+var getElementsByClassName = function(className) {
   // your code here
+  var results = [];
+
+  var searchNode = function(node){
+  	if( node.classList.contains( className )){
+  		results.push( node );
+  	}
+  	for( var i = 0; i < node.children.length; i++ ){
+  		searchNode(node.children[i]);
+  	}
+  };
+
+  searchNode( document.body );
+  return results;
 };
