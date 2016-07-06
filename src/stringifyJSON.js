67e009914fe2a00ebf1b13a8e2e69ccc816ec275
Daniel Metzger <kinaharo@gmail.com>
2016-07-05 20:58:11 -0700

    Return same results as JSON.stringify

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,5 +4,37 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  // your code goes here
+      var output = '';
+
+      if(obj === null ){
+      	output = 'null';
+      }else if(typeof obj === 'boolean'){
+      	output += obj;
+      }else if(typeof obj === 'function'){
+      	output = '{}';
+      }else if(typeof obj === 'string'){
+      	output = '"'+obj+'"';
+      }else if(typeof obj === 'number'){
+      	output+= obj;
+      }else if(Array.isArray(obj)){
+      	for(var i = 0; i<obj.length; i++){
+      		output+=stringifyJSON(obj[i]);
+      		if(obj[i+1]!=undefined){
+      			output+=',';
+      		}
+      	}
+      	output = '[' + output + ']';
+      }else if(typeof obj === 'object'){
+      	for(var x in obj){
+      		if(typeof obj[x] === 'function'){
+      			output += '';
+      		}else if(obj[x] === undefined){
+      			output += '';
+      		}else{
+      			output += stringifyJSON(x) +':'+stringifyJSON(obj[x])+',';
+      		}
+      	}
+      	output = '{' + output.slice(0,(output.length-1)) + '}';
+      }
+      return output;
 };
